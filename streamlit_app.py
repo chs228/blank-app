cf9646e7c86b29175a5a028ee9541e8f113ae656
chs228 <chsubhash22@gmail.com>
2025-04-14 11:16:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -721,8 +721,7 @@ with chat_container:
     
     if user_input := st.chat_input("Type here"):
         process_user_input(user_input)
-        st.rerun()
-
+            st.experimental_rerun()
 # Export on completion
 if st.session_state.interview_complete:
     with st.sidebar:
